ba2aadbf1040d66f4120c0931461678ff87750df
claytoneast <claytoneast@gmail.com>
2016-09-29 22:19:45 -0700

    add servo control logic

---
+++ REAMDE.md
@@ -0,0 +1,8 @@
+## The Alarm Clock of Destiny
+
+Arduino sketch for controlling a servo based on the time of day.   
+Designed to slowly fade in an LED dimmer switch a la https://www.amazon.com/gp/product/B004RZUX8S/ref=oh_aui_detailpage_o06_s00?ie=UTF8&psc=1 at the proper time of day for waking up. Will be using a DS3231 RTC module for time-keeping.
+
+Built in Platform.IO to take advantage of the Atom text editor integration, since the Arduino IDE is trash.
+
+Easily the greatest waste of time yet in my life. Note to self: Buy a Philips Hue bulb next time.
---
+++ src/alarm_clock.cpp
@@ -0,0 +1,49 @@
+#include <Arduino.h>
+#include <Servo.h>
+#include <../lib/Time.cpp>
+int pos = 0;
+
+Servo myservo;  // create servo object to control a servo
+// twelve servo objects can be created on most boards
+
+void printDigits(int digits){
+  // utility function for digital clock display: prints preceding colon and leading 0
+  Serial.print(":");
+  if(digits < 10)
+    Serial.print('0');
+  Serial.print(digits);
+}
+
+void digitalClockDisplay(){
+  // digital clock display of the time
+  Serial.print(hour());
+  printDigits(minute());
+  printDigits(second());
+  Serial.print(" ");
+  Serial.print(day());
+  Serial.print(" ");
+  Serial.print(month());
+  Serial.print(" ");
+  Serial.print(year());
+  Serial.println();
+}
+
+void setup() {
+  myservo.attach(9);  // attaches the servo on pin 9 to the servo object
+  Serial.begin(9600);
+  Serial.print("We have initialized");
+}
+
+void loop() {
+  digitalClockDisplay();
+  for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
+    // in steps of 1 degree
+    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(30);                       // waits 15ms for the servo to reach the position
+  }
+  for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
+    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(30);                       // waits 15ms for the servo to reach the position
+  }
+  while(true);
+}
